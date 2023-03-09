02e4aac03da91ea744790f82ec92d318ee5c79cd
Julian <julian.glaenzer@gmail.com>
2023-03-09 10:48:01 +0100

    c

--- main.py
+++ main.py
@@ -14,10 +14,6 @@ session.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)
 while True:
     try:
         response = session.get(url, cookies=cookies)
-
-        #save html file
-        with open("html.html", "w", encoding="utf-8") as f:
-            f.write(response.text)
         soup = BeautifulSoup(response.text, "html.parser")
 
         target_tr = soup.select_one(f'tr:has(td.tabelle1:-soup-contains("{MODCODE}"))')
